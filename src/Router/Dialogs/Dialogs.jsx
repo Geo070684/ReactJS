ba539ec9183f5cf768d12f7faffacbba38517691
George Erokhin <Georgii070684@gmail.com>
2022-08-10 18:05:36 +0300

    feat: add 21 lesson

--- src/Router/Dialogs/Dialogs.jsx
+++ src/Router/Dialogs/Dialogs.jsx
@@ -2,8 +2,22 @@
 import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
-  return (<div>
-    Dialogs
+  return (<div className={s.dialogs}>
+    <div className={s.dialogsItems}>
+      <div className={s.dialog + " " + s.active}>Bad</div>
+      <div className={s.dialog}>Sup</div>
+      <div className={s.dialog}>Grec</div>
+      <div className={s.dialog}>Petr</div>
+      <div className={s.dialog}>Grud</div>
+      <div className={s.dialog}>Seric</div>
+      <div className={s.dialog}>Perec</div>
+      <div className={s.dialog}>Bob</div>
+    </div>
+    <div className={s.messages}>
+      <div className={s.message}>Hi!</div>
+      <div className={s.message}>How old are you</div>
+      <div className={s.message}>Where are you from</div>
+    </div>
   </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom"
+
 
 const Navbar = () => {
   return (<nav className={s.nav}>
     <div className={s.item}>
-      <a href="/profile">Profile</a>
+      <NavLink to="/profile" className = {classN =>classN.isActive? s.activeLink: null}>Profile</NavLink>
     </div>
     <div className={s.item}>
-      <a href="/dialogs">Messages</a>
+      <NavLink to="/dialogs" className = {navData => navData.isActive? s.activeLink: null}>Messages</NavLink>
     </div>
     <div className={s.item}>
-      <a href="/news">News</a>
+      <NavLink to="/news" className = {navData => navData.isActive? s.activeLink: null}>News</NavLink>
     </div>
     <div className={s.item}>
-      <a href="/music">Music</a>
+      <NavLink to="/music" className = {navData => navData.isActive? s.activeLink: null}>Music</NavLink>
     </div>
     <div className={s.item}>
-      <a href="/setting">Setting</a>
+      <NavLink to="/setting" className = {navData => navData.isActive? s.activeLink: null}>Setting</NavLink>
     </div>
   </nav>)
 }
